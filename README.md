58074bf53ed36c5f353332bdd92c171c07f02dcf
Naren Mudivarthy <nmudivar@cisco.com>
2019-02-27 01:02:32 -0800

    made some changes

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ Cd to the git repr root and execute below cmd
 ```
 pyang --plugindir plugins -f my-plugin models/napalm-star-wars.yang
 ```
+
+## Generating pybind for an yag model
+
+```
+export PYBINDPLUGIN=`/usr/bin/env python3 -c 'import pyangbind; import os; print ("{}/plugin".format(os.path.dirname(pyangbind.__file__)))'`
+pyang --plugindir $PYBINDPLUGIN -f pybind models/napalm-star-wars.yang > napalm_star_wars.py
+```
+
+## References
+
+- [yang-for-dummies](https://napalm-automation.net/yang-for-dummies/)
--- plugins/myplugin.py
+++ plugins/myplugin.py
@@ -8,6 +8,7 @@ from pyang import statements
 def pyang_plugin_init():
     plugin.register_plugin(MyPluginPyangPlugin())
 
+
 class MyPluginPyangPlugin(plugin.PyangPlugin):
     def __init__(self):
         plugin.PyangPlugin.__init__(self, 'my-plugin')
@@ -19,27 +20,30 @@ class MyPluginPyangPlugin(plugin.PyangPlugin):
     def add_opts(self, optparser):
         # you can add additional cmd line options here.
         optlist = [
-            optparse.make_option("--my-plugin-help",
-                                 dest="my_plugin_help",
-                                 action="store_true",
-                                 help="Print help on my-plugin symbols and exit"),
-            optparse.make_option("--my-plugin-tree-path",
-                                 dest="my_plugin_tree_path",
-                                 help="Subtree to print"),
-            optparse.make_option("--my-plugin-tree-no-expand-uses",
-                                 dest="my_plugin_tree_no_expand_uses",
-                                 action="store_false",
-                                 default=False,
-                                 help="Do not expand uses of groupings"),
+            optparse.make_option(
+                "--my-plugin-help",
+                dest="my_plugin_help",
+                action="store_true",
+                help="Print help on my-plugin symbols and exit"),
+            optparse.make_option(
+                "--my-plugin-tree-path",
+                dest="my_plugin_tree_path",
+                help="Subtree to print"),
+            optparse.make_option(
+                "--my-plugin-tree-no-expand-uses",
+                dest="my_plugin_tree_no_expand_uses",
+                action="store_false",
+                default=False,
+                help="Do not expand uses of groupings"),
         ]
         g = optparser.add_option_group("my-plugin output specific options")
         g.add_options(optlist)
-    
+
     def setup_ctx(self, ctx):
         if ctx.opts.my_plugin_help:
             print_help()
             sys.exit(0)
-   
+
     def setup_fmt(self, ctx):
         ctx.implicit_errors = False
 
@@ -53,22 +57,65 @@ class MyPluginPyangPlugin(plugin.PyangPlugin):
         emit_tree(ctx, modules, fd, ctx.opts.tree_depth,
                   ctx.opts.tree_line_length, path)
 
+
+class Metrics:
+    def __init__(self, basepath):
+        self.basepath = basepath
+        self.spec = {"fields": list()}
+
+    def append(self, data):
+        self.spec['fields'].append(data)
+
+    def __repr__(self):
+        return self.basepath + ' ' + str(self.spec)
+
+    def print(self):
+        print(self.basepath)
+        for f in self.spec['fields']:
+            print('    ', f)
+
+class Metric:
+    def __init__(self, name):
+        self.name = name
+        self.fields = list()
+
+    def append(self, metric):
+        if metric not in self.fields:
+            self.fields.append(metric)
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return (self.name == other.name)
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __repr__(self):
+        return "{'name': %s, 'fields': %s}" % (self.name, str(self.fields))
+
+    def __contains__(self, metric):
+        for m in self.fields:
+            if m == metric:
+                return True
+
+        return False
+
+    def print(self):
+        print(self.name)
+        for f in self.fields:
+            print('    ', f)
+
+
 def emit_tree(ctx, modules, fd, depth, llen, path):
     print('Emitting tree dummy')
-
+    pathstr = ''
+    metrics = Metrics(basepath=pathstr)
     for module in modules:
-        printed_header = False
-
-        def print_header():
-            bstr = ""
-            b = module.search_one('belongs-to')
-            if b is not None:
-                bstr = " (belongs-to %s)" % b.arg
-            fd.write("%s: %s%s\n" % (module.keyword, module.arg, bstr))
-            printed_header = True
-
-        chs = [ch for ch in module.i_children
-               if ch.keyword in statements.data_definition_keywords]
+        chs = [
+            ch for ch in module.i_children
+            if ch.keyword in statements.data_definition_keywords
+        ]
 
         if path is not None and len(path) > 0:
             chs = [ch for ch in chs if ch.arg == path[0]]
@@ -78,27 +125,123 @@ def emit_tree(ctx, modules, fd, depth, llen, path):
 
         print("chpath", chpath)
 
-        if len(chs) > 0:
-            if not printed_header:
-                print_header()
-        print_children(chs, module, fd, '', chpath, 'data', depth, llen,
-                           ctx.opts.my_plugin_tree_no_expand_uses,
-                           prefix_with_modname=ctx.opts.modname_prefix)
+        print_children(
+            chs,
+            module,
+            fd,
+            chpath,
+            'data',
+            depth,
+            llen,
+            ctx.opts.my_plugin_tree_no_expand_uses,
+            prefix_with_modname=ctx.opts.modname_prefix,
+            metrics=metrics)
+        print('--'*20)
+        print(metrics)
+        print('--'*20)
 
-def print_children(i_children, module, fd, prefix, path, mode, depth,
-                   llen, no_expand_uses=False, width=0, prefix_with_modname=False):
-    print(i_children)
-    print(prefix)
-    print(path)
-    print(depth)
-    print(no_expand_uses)
-
+# metric
+def print_children(i_children,
+                   module,
+                   fd,
+                   path,
+                   mode,
+                   depth,
+                   llen,
+                   no_expand_uses=False,
+                   width=0,
+                   prefix_with_modname=False,
+                   metrics=None,
+                   metric=None):
     if depth == 0:
-        if i_children: fd.write(prefix + '     ...\n')
+        if i_children: fd.write(' depth 0 case    ...\n')
         return
-    
+
+    def get_width(w, chs):
+        for ch in chs:
+            if ch.keyword in ['choice', 'case']:
+                nlen = 3 + get_width(0, ch.i_children)
+            else:
+                if ch.i_module.i_modulename == module.i_modulename:
+                    nlen = len(ch.arg)
+                else:
+                    nlen = len(ch.i_module.i_prefix) + 1 + len(ch.arg)
+            if nlen > w:
+                w = nlen
+        return w
+
     if no_expand_uses:
         i_children = unexpand_uses(i_children)
+
+    if width == 0:
+        width = get_width(0, i_children)
+
+    for ch in i_children:
+        if ((ch.keyword == 'input' or ch.keyword == 'output')
+                and len(ch.i_children) == 0):
+            continue
+        print_node(
+            ch,
+            module,
+            fd,
+            path,
+            mode,
+            depth,
+            llen,
+            no_expand_uses,
+            width,
+            prefix_with_modname=prefix_with_modname,
+            metric=metric)
+
+
+def print_node(s,
+               module,
+               fd,
+               path,
+               mode,
+               depth,
+               llen,
+               no_expand_uses,
+               width,
+               prefix_with_modname=False,
+               metric=None):
+    if s.i_module.i_modulename == module.i_modulename:
+        name = s.arg
+    else:
+        name = s.i_module.i_prefix + ':' + s.arg
+
+    m = Metric(name)
+
+    if s.keyword == 'container':
+        if metric is None:
+            metric = m
+        else:
+            metric.append(m)
+    elif s.keyword == 'leaf':
+        metric.append(Metric(name))
+
+    print(s.keyword, name)
+    print('In Print node', metric)
+
+    if hasattr(s, 'i_children') and s.keyword != 'uses':
+        if depth is not None:
+            depth -= 1
+        chs = s.i_children
+        if path is not None and len(path) > 0:
+            chs = [ch for ch in chs if ch.arg == path[0]]
+            path = path[1:]
+        print_children(
+                chs,
+                module,
+                fd,
+                path,
+                mode,
+                depth,
+                llen,
+                no_expand_uses,
+                prefix_with_modname=prefix_with_modname,
+                metric=metric)
+
 
 def unexpand_uses(i_children):
     res = []
@@ -113,6 +256,7 @@ def unexpand_uses(i_children):
         else:
             res.append(ch)
     return res
+
 
 def print_help():
     print("""
